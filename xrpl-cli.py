472a4caa8d4036b50e23af5eba37e10df1ae3f5e
covecredit <matthew@hacker.house>
2022-04-05 13:57:40 -0500

    arg handling

--- xrpl-cli.py
+++ xrpl-cli.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# xrpl-cli - a command line tool for working with the XRPL ledger
 import sys
 import json
 import argparse
@@ -13,7 +14,6 @@ JSON_RPC_URL = "http://xls20-sandbox.rippletest.net:51234"
 #mint
 #get
 #burn
-
 #create_sell
 #create_buy
 #get_offers
@@ -21,19 +21,18 @@ JSON_RPC_URL = "http://xls20-sandbox.rippletest.net:51234"
 #accept_buy
 #cancel_offer
 
-#--account
-#--secret
-#--tokenurl
-#--flags
-#--tokenid
-#--amount
-#--tokenofferindex
-#--owner
-
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser()
-	parser.add_argument("account")
-	args = parser.parse_args()
+	parser = argparse.ArgumentParser(description="A command-line interface for working with the XRPL ledger")
+	parser.add_argument("-a","--account", help="classic account address")
+	parser.add_argument("-s","--secret", help="seed key")
+	parser.add_argument("-t","--tokenurl", help="NFT token url")
+	parser.add_argument("-f","--flags", help="NFT flags")
+	parser.add_argument("-i","--tokenid", help="token id")
+	parser.add_argument("-m","--amount", help="amount")
+	parser.add_argument("-x","--tokenofferindex", help="tokenofferindex")
+	parser.add_argument("-o","--owner", help="owner")
+
+# query the account arg.
 	client = JsonRpcClient(JSON_RPC_URL)
 	acct_info = AccountInfo(account=args.account,ledger_index="validated",strict=True)
 	response = client.request(acct_info)
