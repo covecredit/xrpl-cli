76a2c757057ea3e738af3adea0878e46113d8eaf
covecredit <matthew@hacker.house>
2022-08-31 11:49:34 -0500

    mint.py now mints an NFT on xls-20 sandbox

--- mint.py
+++ mint.py
@@ -6,10 +6,12 @@ import argparse
 from xrpl.clients import JsonRpcClient
 from xrpl.wallet import generate_faucet_wallet
 from xrpl.core import addresscodec
-from xrpl.transaction import send_reliable_submission, safe_sign_and_autofill_transaction
+from xrpl.transaction import send_reliable_submission, safe_sign_and_autofill_transaction, safe_sign_transaction
 from xrpl.models.requests.account_info import AccountInfo
 from xrpl.models.transactions import NFTokenMint
 from xrpl.models.requests import AccountNFTs
+from xrpl.ledger import get_latest_validated_ledger_sequence
+from xrpl.account import get_next_valid_seq_number
 
 if __name__ == "__main__":
     JSON_RPC_URL = "http://xls20-sandbox.rippletest.net:51234"
@@ -33,9 +35,14 @@ if __name__ == "__main__":
     print("response.status: ", response.status)
     print(json.dumps(response.result, indent=4, sort_keys=True))
 
+    # get the current block height 
+    current_validated_ledger = get_latest_validated_ledger_sequence(client)
+    test_wallet.sequence = get_next_valid_seq_number(test_wallet.classic_address, client)
+
+
     # nft_mint uri must be > 5 chars
     # nftoken_taxon = Required, but if you have no use for it, set to zero.
-    nft_mint = NFTokenMint(account=test_wallet.classic_address, nftoken_taxon=0, uri="\x41\x42\x43\x44\x45")
+    nft_mint = NFTokenMint(account=test_wallet.classic_address, nftoken_taxon=0, uri="4142434445")
     print(nft_mint)
     print(nft_mint.is_valid())
     nft_mint_signed = safe_sign_and_autofill_transaction(nft_mint, test_wallet, client)
